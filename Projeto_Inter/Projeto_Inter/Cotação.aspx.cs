19d7e71f4fccbed25bb66ad54006fb1964b8cfcb
Renan Freitas <Renan Freitas@DESKTOP-43PI9TG>
2018-05-05 10:52:27 -0300

    lucas inter cotaçao

--- Projeto_Inter/Projeto_Inter/Cotação.aspx.cs
+++ Projeto_Inter/Projeto_Inter/Cotação.aspx.cs
@@ -15,7 +15,7 @@ namespace Projeto_Inter
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            //lucas rossi
         }
         public void LimparCampos()
         {
